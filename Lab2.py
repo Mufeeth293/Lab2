91d4f27736b13098143aafd86b03a4b6219b3811
Mufeeth <MUFEETHMUFAZZAL.24@ichat.sp.edu.sg>
2025-10-29 11:59:32 +0800

    added functions

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,44 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+# def display_main_menu():
+#     print("Enter some numbers separated by commas (e.g. 5, 67,32)")
+
+
+def get_user_input():  
+    userinput = input()
+    string_list = userinput.split(",")
+   # print (string_list )
+    float_list = []
+    for string in string_list:
+        float_num = float(string)
+        float_list.append(float_num)
+
+    #print("float_list", float_list)
+    return float_list
+
+
+
+
+def calc_average_temperature(flist):
+    s = sum(flist)
+    avg = s/len(flist)
+    print(round(avg,2))
+
+def calc_min_max_temperature(flist):
+    maxi = max(flist)
+    minu = min(flist)
+    l = [maxi,minu]
+    print(l)
+
+   
+def main():
+    l = get_user_input()
+    calc_average_temperature(l)
+    calc_min_max_temperature(l)
+
+if __name__ == "__main__":
+    main()
+# display_main_menu()
+
+# # print(res)
+
+
+
